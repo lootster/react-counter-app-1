4b30fe590eb41d752f531e469eb3d7e8cb1ac30a
yaminmhd <mdyamin.drzee@gmail.com>
2018-09-30 23:41:17 +0800

    implement single source of truth

--- src/Button.js
+++ src/Button.js
@@ -1,16 +1,30 @@
 import React from "react";
 
-const Button = ({ children, classType, clickHandler, onDelete, id }) => {
+const Button = ({
+  children,
+  classType,
+  onAdd,
+  onMinus,
+  counter,
+  onDelete
+}) => {
   return onDelete ? (
     <button
-      onClick={() => onDelete(id)}
+      onClick={() => onDelete(counter.id)}
+      className={`btn btn-${classType} btn-lg m-2`}
+    >
+      {children}
+    </button>
+  ) : onAdd ? (
+    <button
+      onClick={() => onAdd(counter)}
       className={`btn btn-${classType} btn-lg m-2`}
     >
       {children}
     </button>
   ) : (
     <button
-      onClick={clickHandler}
+      onClick={() => onMinus(counter)}
       className={`btn btn-${classType} btn-lg m-2`}
     >
       {children}
--- src/Counter.js
+++ src/Counter.js
@@ -3,50 +3,26 @@ import Label from "./Label";
 import Button from "./Button";
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.counter.value
-    };
-    this.add = this.add.bind(this);
-    this.minus = this.minus.bind(this);
-  }
-
-  add() {
-    //this.props.counter = 0; don't change state directly
-    this.setState(prevState => {
-      return {
-        value: prevState.value + 1
-      };
-    });
-  }
-
-  minus() {
-    this.setState(prevState => {
-      return {
-        value: prevState.value - 1
-      };
-    });
-  }
-
   render() {
-    const { value } = this.state;
     const {
-      counter: { name, id },
-      onDelete
+      counter,
+      counter: { name, value },
+      onDelete,
+      onAdd,
+      onMinus
     } = this.props;
     return (
       <div className="border border-warning rounded center">
         <span className="badge badge-info">{name}</span>
         <Label value={value} />
-        <h1 className="big">{this.state.value}</h1>
-        <Button classType="primary" clickHandler={this.add}>
+        <h1 className="big">{value}</h1>
+        <Button classType="primary" onAdd={onAdd} counter={counter}>
           +
         </Button>
-        <Button classType="danger" clickHandler={this.minus}>
+        <Button classType="danger" onMinus={onMinus} counter={counter}>
           -
         </Button>
-        <Button classType="danger" onDelete={onDelete} id={id}>
+        <Button classType="danger" onDelete={onDelete} counter={counter}>
           Delete
         </Button>
       </div>
