61c74aa77f2ca8330a2ea35bc5a2ac986a7ace21
yaminmhd <mdyamin.drzee@gmail.com>
2018-09-30 14:25:38 +0800

    first requirement

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Counter = () => {
-  return (
-    <div>
-      <h1 className="big">Value</h1>
-    </div>
-  );
-};
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.add = this.add.bind(this);
+    this.minus = this.minus.bind(this);
+  }
+
+  add() {
+    this.setState(prevState => {
+      return {
+        value: prevState.value + 1
+      };
+    });
+  }
+
+  minus() {
+    this.setState(prevState => {
+      return {
+        value: prevState.value - 1
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="border border-warning rounded center">
+        <h1 className="big">{this.state.value}</h1>
+        <button className="btn btn-primary btn-lg m-2" onClick={this.add}>
+          +
+        </button>
+        <button className="btn btn-danger btn-lg m-2" onClick={this.minus}>
+          -
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Counter;
